b119f48cc7c67cd33e3d9b0fc888e9654e7914b4
PieroArbildo <89119204+PieroArbildo@users.noreply.github.com>
2024-06-26 22:42:54 -0500

    w2 problem2 completed

--- week02/code/Priority.cs
+++ week02/code/Priority.cs
@@ -1,30 +1,70 @@
-﻿public static class Priority {
-    public static void Test() {
-        // TODO Problem 2 - Write and run test cases and fix the code to match requirements
-        // Example of creating and using the priority queue
-        var priorityQueue = new PriorityQueue();
-        Console.WriteLine(priorityQueue);
+﻿public static class Priority
+{
+  public static void Test()
+  {
+    // TODO Problem 2 - Write and run test cases and fix the code to match requirements
+    // Example of creating and using the priority queue
+    var priorityQueue = new PriorityQueue();
+    priorityQueue.Enqueue("tim", 2);
+    priorityQueue.Enqueue("tom", 3);
+    priorityQueue.Enqueue("tum", 1);
+    priorityQueue.Enqueue("tiny", 8);
+    priorityQueue.Enqueue("tuna", 9);
+    priorityQueue.Enqueue("tunes", 3);
+    priorityQueue.Enqueue("tany", 0);
 
-        // Test Cases
+    var priorityQueue2 = new PriorityQueue();
+    priorityQueue2.Enqueue("tim", 6);
+    priorityQueue2.Enqueue("tom", 3);
 
-        // Test 1
-        // Scenario: 
-        // Expected Result: 
-        Console.WriteLine("Test 1");
+    // Test Cases
 
-        // Defect(s) Found: 
+    // Test 1
+    // Scenario: Order elements by priority number when using the Enqueue method
+    // Expected Result: [tany (Pri:0), tum (Pri:1), tim (Pri:2), tom (Pri:3), tunes (Pri:3), tiny (Pri:8), tuna (Pri:9)]
+    // Defect(s) Found: There was no code that ordered by priority number inside the "Enqueue" method.
+    Console.WriteLine("Test 1");
 
-        Console.WriteLine("---------");
+    Console.WriteLine(priorityQueue);
+    
+    Console.WriteLine("---------");
 
-        // Test 2
-        // Scenario: 
-        // Expected Result: 
-        Console.WriteLine("Test 2");
+    // Test 2
+    // Scenario: Delete and print element with highest priority using Dequeue method
+    // Expected Result: tuna
+    // Defect(s) Found: It printed the highest priority -1 which made it not the last,
+    // RemoveAt method was added that takes the last index
+    Console.WriteLine("Test 2");
 
-        // Defect(s) Found: 
+    Console.WriteLine(priorityQueue.Dequeue());
 
-        Console.WriteLine("---------");
+    Console.WriteLine("---------");
 
-        // Add more Test Cases As Needed Below
-    }
+    // Test 3
+    // Scenario: add elements with the same priority and delete to verify that FIFO is met
+    // Expected Result: [tim (Pri:5), tom (Pri:5)]
+    Console.WriteLine("Test 3");
+
+    var priorityQueue3 = new PriorityQueue();
+    priorityQueue3.Enqueue("tim", 5);
+    priorityQueue3.Enqueue("tom", 5);
+    priorityQueue3.Enqueue("tany", 5);
+    Console.WriteLine(priorityQueue3);
+    Console.WriteLine(priorityQueue3.Dequeue());
+    Console.WriteLine(priorityQueue3);
+    Console.WriteLine("---------");
+
+    // Test 4
+    // Scenario: Remove elements from the list, and throw an error when there are no more elements to remove
+    // Expected Result: tim, tom, throw Error
+    // Defect(s) Found: There was no exception code, instead there was a message "The queue is empty".
+    Console.WriteLine("Test 4");
+
+    Console.WriteLine(priorityQueue2.Dequeue());
+    Console.WriteLine(priorityQueue2.Dequeue());
+    Console.WriteLine(priorityQueue2.Dequeue());
+    Console.WriteLine("---------");
+
+    // Add more Test Cases As Needed Below
+  }
 }
--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -1,4 +1,5 @@
-﻿public class PriorityQueue {
+﻿public class PriorityQueue
+{
     private List<PriorityItem> _queue = new();
 
     /// <summary>
@@ -8,45 +9,55 @@
     /// </summary>
     /// <param name="value">The value</param>
     /// <param name="priority">The priority</param>
-    public void Enqueue(string value, int priority) {
+    public void Enqueue(string value, int priority)
+    {
         var newNode = new PriorityItem(value, priority);
         _queue.Add(newNode);
+        _queue.Sort((x, y) => x.Priority.CompareTo(y.Priority)); // Sort by priority
     }
 
-    public String Dequeue() {
+    public String Dequeue()
+    {
         if (_queue.Count == 0) // Verify the queue is not empty
         {
-            Console.WriteLine("The queue is empty.");
-            return null;
+            //Console.WriteLine("The queue is empty.");
+            throw new InvalidOperationException("No one in the queue.");
+            //return null;
         }
 
         // Find the index of the item with the highest priority to remove
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++) {
+        for (int index = 1; index < _queue.Count; index++)
+        { // _queue.Count -1 The "-1" was removed so that it does not take the penultimate highest priority, but rather the last highest
             if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
                 highPriorityIndex = index;
         }
 
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex); // RemoveAt method was added that takes the last index
         return value;
     }
 
-    public override string ToString() {
+    public override string ToString()
+    {
         return $"[{string.Join(", ", _queue)}]";
     }
 }
 
-internal class PriorityItem {
+internal class PriorityItem
+{
     internal string Value { get; set; }
     internal int Priority { get; set; }
 
-    internal PriorityItem(string value, int priority) {
+    internal PriorityItem(string value, int priority)
+    {
         Value = value;
         Priority = priority;
     }
 
-    public override string ToString() {
+    public override string ToString()
+    {
         return $"{Value} (Pri:{Priority})";
     }
 }
